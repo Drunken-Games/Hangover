348392d18592ecacc7855716ad78a71adfb7f811
박봉균 <supermmkpk@gmail.com>
2024-10-28 10:46:18 +0900

    build: springboot API 프로젝트 초기 세팅 #S11P31C205-56 #in-progress

---
+++ HangoverAPI/src/test/java/com/drunkengames/hangover/HangoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drunkengames.hangover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
